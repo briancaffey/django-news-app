005efdbee32ec000dc67c9ea3baef3c7bb1eb9da
Brian Caffey <briancaffey2010@gmail.com>
2018-02-09 21:42:18 -0500

    initial commit

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/brian/Documents/interviews/dryden/django-app/dryden/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ virtualenv/lib/python3.6/enum.py
@@ -0,0 +1,1 @@
+/home/brian/anaconda3/lib/python3.6/enum.py
